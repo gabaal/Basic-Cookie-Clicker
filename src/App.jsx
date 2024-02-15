f3d18cdbe428a124535305aa20e355925872ef07
gabaal <geraldbaalham@live.co.uk>
2024-02-15 13:42:51 +0000

    Upgrades double in cookies cost when used

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,28 @@ import ResetButton from './ResetButton.jsx'
 import Upgrades from './Upgrades.jsx'
 
 function App() {
-  const [cookies, setCookies] = useState(0);
-  const [cookiesPerSecond, setCookiesPerSecond] = useState(1);
-  const [upgrades, setUpgrades] = useState([
+  const initialUpgrades = [
     { id: 1, name: 'Upgrade 1', cost: 10, cookiesPerSecondIncrease: 1 },
     { id: 2, name: 'Upgrade 2', cost: 20, cookiesPerSecondIncrease: 2 },
     { id: 3, name: 'Upgrade 3', cost: 30, cookiesPerSecondIncrease: 3 },
     { id: 4, name: 'Upgrade 4', cost: 40, cookiesPerSecondIncrease: 4 },
     { id: 5, name: 'Upgrade 5', cost: 50, cookiesPerSecondIncrease: 5 },
-  ]);
+  ];
+
+  const [cookies, setCookies] = useState(0);
+  const [cookiesPerSecond, setCookiesPerSecond] = useState(1);
+  const [upgrades, setUpgrades] = useState(initialUpgrades);
 
   // Function to handle clicking on the cookie
   const handleClickCookie = () => {
     setCookies(cookies + 1);
   };
 
-  // Function to reset the number of cookies to zero
+  // Function to reset the number of cookies, cookies per second, and upgrades to their initial values
   const handleReset = () => {
     setCookies(0);
+    setCookiesPerSecond(1);
+    setUpgrades(initialUpgrades);
   };
 
   // Function to purchase an upgrade
@@ -34,7 +38,10 @@ function App() {
     if (cookies >= cost) {
       setCookies(cookies - cost);
       setCookiesPerSecond(cookiesPerSecond + cookiesPerSecondIncrease);
-      setUpgrades(upgrades.filter(upgrade => upgrade.id !== id));
+      const newUpgrades = upgrades.map(upgrade =>
+        upgrade.id === id ? { ...upgrade, cost: upgrade.cost * 2 } : upgrade
+      );
+      setUpgrades(newUpgrades);
     } else {
       alert('Not enough cookies to purchase this upgrade!');
     }
@@ -50,18 +57,16 @@ function App() {
             <p>Cookies: {cookies}</p>
           </div>
           <div>
-            <CookiesPerSecond cookies={cookies} setCookies={setCookies} cookiesPerSecond={cookiesPerSecond} /> {/* Use the CookiesPerSecond component */}
+            <CookiesPerSecond cookiesPerSecond={cookiesPerSecond} />
           </div>
           <div>
             <CookieClicked handleClick={handleClickCookie} />
           </div>
           <Upgrades upgrades={upgrades} purchaseUpgrade={purchaseUpgrade} cookies={cookies} />
-          <CookiesUpdater cookies={cookies} setCookies={setCookies} cookiesPerSecond={cookiesPerSecond} /> {/* Use the CookiesUpdater component */}
+          <CookiesUpdater cookies={cookies} setCookies={setCookies} cookiesPerSecond={cookiesPerSecond} />
         </div>
       </header>
     </div>
   );
 }
-
-
 export default App;
